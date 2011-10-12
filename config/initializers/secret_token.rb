3b9f3ac8ecc1cd0e0ccf1b99fea2b6a17cbc41df
subbuballa <balu.003@gmail.com>
2011-10-12 01:14:33 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Insight::Application.config.secret_token = '1e5031966e5ab5ef0a06818194b43c824b6a3626f0f5a54d90b6ec9911777085d2bc2ca3b96b14380376a18a5ec6db2912902c8cf33ee2022f65a3c0d65addc3'
